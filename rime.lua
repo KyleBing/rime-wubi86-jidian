1dc70fa3e896cd6f573a150ae484784f1a27344f
KyleBing <kylebing@163.com>
2019-12-04 13:46:47 +0800

    添加日期候选

--- rime.lua
+++ rime.lua
@@ -1,7 +1,8 @@
 function date_translator(input, seg)
    if (input == "date") then
       --- Candidate(type, start, end, text, comment)
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), "日期"))
+      yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), "年月日"))
+      yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), "Y-M-D"))
    end
    if (input == "time") then
       --- Candidate(type, start, end, text, comment)
