60821a9f38c9b1bdbeacd2fd3680a07b1fd64d1e
KyleBing <kylebing@163.com>
2020-05-12 15:37:29 +0800

    update lua

--- rime.lua
+++ rime.lua
@@ -1,14 +1,16 @@
 function date_translator(input, seg)
    if (input == "date") then
       --- Candidate(type, start, end, text, comment)
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), "年-月-日"))
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), "年月日"))
+      yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
+      yield(Candidate("date", seg.start, seg._end, os.date("%m-%d"), ""))
+      yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
+      yield(Candidate("date", seg.start, seg._end, os.date("%m-%d-%Y"), ""))
    end
    if (input == "time") then
       --- Candidate(type, start, end, text, comment)
-      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), "时分秒"))
-      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), "时分"))
-	  yield(Candidate("time", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), "长时间"))
+      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
+      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), ""))
+	  yield(Candidate("time", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), ""))
    end
 end
 
