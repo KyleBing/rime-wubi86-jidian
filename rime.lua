608ebfc2931d1ce66ecadf73a9825ab76d9835cd
KyleBing <kylebing@163.com>
2020-05-29 15:44:55 +0800

    3个候选，编码显示

--- rime.lua
+++ rime.lua
@@ -5,6 +5,7 @@ function date_translator(input, seg)
       yield(Candidate("date", seg.start, seg._end, os.date("%m-%d"), ""))
       yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
       yield(Candidate("date", seg.start, seg._end, os.date("%m-%d-%Y"), ""))
+      yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
    end
    if (input == "time") then
       --- Candidate(type, start, end, text, comment)
