b37b1536467c665c7a78f350a42d36da7421c891
KyleBing <kylebing@163.com>
2020-11-06 21:59:38 +0800

    ^

--- rime.lua
+++ rime.lua
@@ -1,31 +1,31 @@
 function date_translator(input, seg)
-   if (input == "date") then
-      --- Candidate(type, start, end, text, comment)
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
-      yield(Candidate("date", seg.start, seg._end, os.date("%m-%d"), ""))
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
-      yield(Candidate("date", seg.start, seg._end, os.date("%m-%d-%Y"), ""))
-      yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
-   end
-   if (input == "time") then
-      --- Candidate(type, start, end, text, comment)
-      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
-      yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), ""))
-	  yield(Candidate("time", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), ""))
-   end
+    if (input == "date") then
+        --- Candidate(type, start, end, text, comment)
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%m-%d"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%m-%d-%Y"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
+    end
+    if (input == "time") then
+        --- Candidate(type, start, end, text, comment)
+        yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), ""))
+        yield(Candidate("time", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), ""))
+        yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
+    end
 end
 
 --- 过滤器：单字在先
 function single_char_first_filter(input)
-   local l = {}
-   for cand in input:iter() do
-      if (utf8.len(cand.text) == 1) then
-         yield(cand)
-      else
-         table.insert(l, cand)
-      end
-   end
-   for i, cand in ipairs(l) do
-      yield(cand)
-   end
+    local l = {}
+    for cand in input:iter() do
+        if (utf8.len(cand.text) == 1) then
+            yield(cand)
+        else
+            table.insert(l, cand)
+        end
+    end
+    for cand in ipairs(l) do
+        yield(cand)
+    end
 end
