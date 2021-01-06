bb735dce564bf025d6f5497c8e00cf257a73205a
KyleBing <kylebing@163.com>
2021-01-06 13:51:23 +0800

    添加 week，感谢 @JiandanDream https://github.com/KyleBing/rime-wubi86-jidian/issues/54

--- rime.lua
+++ rime.lua
@@ -13,6 +13,15 @@ function date_translator(input, seg)
         yield(Candidate("time", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), ""))
         yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
     end
+
+    -- @JiandanDream
+    -- https://github.com/KyleBing/rime-wubi86-jidian/issues/54
+
+    if (input == "week") then
+        local weakTab = {'日', '一', '二', '三', '四', '五', '六'}
+        yield(Candidate("week", seg.start, seg._end, "周"..weakTab[tonumber(os.date("%w")+1)], ""))
+        yield(Candidate("week", seg.start, seg._end, "星期"..weakTab[tonumber(os.date("%w")+1)], ""))
+    end
 end
 
 --- 过滤器：单字在先
