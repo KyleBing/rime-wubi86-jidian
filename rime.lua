a61588b935d3fa6ef66d7e84c0df8f336c9669c0
邴新科 <kylebing@163.com>
2022-03-11 16:12:34 +0800

    fix https://github.com/KyleBing/rime-wubi86-jidian/issues/71

--- rime.lua
+++ rime.lua
@@ -18,7 +18,7 @@ end
 
 function calculator(input, seg)
     if string.find(input, 'coco') ~= nil then -- 匹配 coco 开头的字符串
-        local _, _, a, operation, b = string.find(input, "coco(%d+%.?%d+)([%+%-%*/])(%d+%.?%d+)")
+        local _, _, a, operation, b = string.find(input, "coco(%d+%.?%d*)([%+%-%*/])(%d+%.?%d*)")
         local result = 0
         if operation == '+' then
             result = a + b
