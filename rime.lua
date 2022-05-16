923f043423676dc7b259cf53f576da1907aefcc7
邴新科 <kylebing@163.com>
2022-05-16 10:15:15 +0800

    week month 添加对应的星期、月份英文输出

--- rime.lua
+++ rime.lua
@@ -35,6 +35,29 @@ end
 
 
 function date_translator(input, seg)
+
+    -- 日期格式说明：
+
+    -- %a	abbreviated weekday name (e.g., Wed)
+    -- %A	full weekday name (e.g., Wednesday)
+    -- %b	abbreviated month name (e.g., Sep)
+    -- %B	full month name (e.g., September)
+    -- %c	date and time (e.g., 09/16/98 23:48:10)
+    -- %d	day of the month (16) [01-31]
+    -- %H	hour, using a 24-hour clock (23) [00-23]
+    -- %I	hour, using a 12-hour clock (11) [01-12]
+    -- %M	minute (48) [00-59]
+    -- %m	month (09) [01-12]
+    -- %p	either "am" or "pm" (pm)
+    -- %S	second (10) [00-61]
+    -- %w	weekday (3) [0-6 = Sunday-Saturday]
+    -- %x	date (e.g., 09/16/98)
+    -- %X	time (e.g., 23:48:10)
+    -- %Y	full year (1998)
+    -- %y	two-digit year (98) [00-99]
+    -- %%	the character `%´
+
+    -- 输入日期
     if (input == "date") then
         --- Candidate(type, start, end, text, comment)
         yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
@@ -43,6 +66,8 @@ function date_translator(input, seg)
         yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%m-%d-%Y"), ""))
     end
+
+    -- 输入时间
     if (input == "time") then
         --- Candidate(type, start, end, text, comment)
         yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), ""))
@@ -50,12 +75,33 @@ function date_translator(input, seg)
         yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
     end
 
-    -- @JiandanDream
-    -- https://github.com/KyleBing/rime-wubi86-jidian/issues/54
 
+    -- 输入星期
+    -- -- @JiandanDream
+    -- -- https://github.com/KyleBing/rime-wubi86-jidian/issues/54
     if (input == "week") then
         local weakTab = {'日', '一', '二', '三', '四', '五', '六'}
         yield(Candidate("week", seg.start, seg._end, "周"..weakTab[tonumber(os.date("%w")+1)], ""))
         yield(Candidate("week", seg.start, seg._end, "星期"..weakTab[tonumber(os.date("%w")+1)], ""))
+        yield(Candidate("week", seg.start, seg._end, os.date("%A"), ""))
+        yield(Candidate("week", seg.start, seg._end, os.date("%a"), "缩写"))
+
+        --local weakTabEnShort = {"Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"}
+        --local weakTabEnFull = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
+        --yield(Candidate("week", seg.start, seg._end, weakTabEnFull[tonumber(os.date("%w")+1)], ""))
+        --yield(Candidate("week", seg.start, seg._end, weakTabEnShort[tonumber(os.date("%w")+1)], "缩写"))
+    end
+
+    -- 输入月份英文
+    if (input == "month") then
+        yield(Candidate("month", seg.start, seg._end, os.date("%B"), ""))
+        yield(Candidate("month", seg.start, seg._end, os.date("%b"), "缩写"))
+
+        --local monthTabEnShort = {"Jan.", "Feb.", "Mar.", "Apr.", "May","Jun.", "Jul.", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."}
+        --local monthTabEnFull = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
+        --yield(Candidate("month", seg.start, seg._end, monthTabEnFull[tonumber(os.date("%m"))], ""))
+        --yield(Candidate("month", seg.start, seg._end, monthTabEnShort[tonumber(os.date("%m"))], "缩写"))
     end
 end
+
+
