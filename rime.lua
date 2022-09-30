f0b630255088e696ae77093ee0a90a072f07c15c
KyleBing <kylebing@163.com>
2022-09-30 20:23:47 +0800

    ^

--- rime.lua
+++ rime.lua
@@ -64,8 +64,8 @@ function date_translator(input, seg)
     -- 输入日期
     if (input == "date") then
         --- Candidate(type, start, end, text, comment)
-        yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%Y.%m.%d"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%m-%d-%Y"), ""))
