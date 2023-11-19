b51900a189af8071671259202bd2478124de0685
e71828 <740943170@qq.com>
2023-11-19 16:28:54 +0800

    consideration for install

---
+++ Windows-使用 plum 安装后复制内容.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cp "$APPDATA"/plum/package/e71828/wubi86-jidian/wubi86_jidian.custom.yaml  "$APPDATA"/Rime/
+cp "$APPDATA"/plum/package/e71828/wubi86-jidian/default.custom.yaml  "$APPDATA"/Rime/
+cp "$APPDATA"/plum/package/e71828/wubi86-jidian/weasel.custom.yaml  "$APPDATA"/Rime/
+cp "$APPDATA"/plum/package/e71828/wubi86-jidian/rime.lua  "$APPDATA"/Rime/
+cp "$APPDATA"/plum/package/e71828/wubi86-jidian/wubi86_jidian.ico  "$APPDATA"/Rime/
+cp "$APPDATA"/plum/package/e71828/wubi86-jidian/preview  "$APPDATA"/Rime/ -r
+
