8d982d628ac48957d514c9cae638b9f64c708edd
e71828 <740943170@qq.com>
2023-11-25 17:29:49 +0800

    个人设定，结合雾凇拼音

--- Windows-使用 plum 安装后复制内容.sh
+++ Windows-使用 plum 安装后复制内容.sh
@@ -2,7 +2,9 @@
 cp "$APPDATA"/plum/package/e71828/wubi86-jidian/wubi86_jidian.custom.yaml  "$APPDATA"/Rime/
 cp "$APPDATA"/plum/package/e71828/wubi86-jidian/default.custom.yaml  "$APPDATA"/Rime/
 cp "$APPDATA"/plum/package/e71828/wubi86-jidian/weasel.custom.yaml  "$APPDATA"/Rime/
-cp "$APPDATA"/plum/package/e71828/wubi86-jidian/rime.lua  "$APPDATA"/Rime/
+cp "$APPDATA"/plum/package/e71828/wubi86-jidian/rime.lua  "$APPDATA"/Rime/jidian.rime.lua
+touch "$APPDATA"/Rime/ice.rime.lua
+diff -DVERSION1 "$APPDATA"/Rime/ice.rime.lua "$APPDATA"/Rime/jidian.rime.lua > "$APPDATA"/Rime/rime.lua
 cp "$APPDATA"/plum/package/e71828/wubi86-jidian/wubi86_jidian.ico  "$APPDATA"/Rime/
 cp "$APPDATA"/plum/package/e71828/wubi86-jidian/preview  "$APPDATA"/Rime/ -r
-
+cp "$APPDATA"/plum/package/e71828/wubi86-jidian/lua  "$APPDATA"/Rime/ -r
